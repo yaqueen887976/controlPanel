38f1cc95d2e6d6c4526b777bf1eef7c2425d9a08
yaqueen887976 <yaqinwang@email.arizona.edu>
2019-12-04 20:10:27 -0700

    product router setup

--- react-admin/src/pages/product/product.jsx
+++ react-admin/src/pages/product/product.jsx
@@ -1,11 +1,20 @@
 import React, {Component} from 'react';
+import {Switch, Route,Redirect} from 'react-router-dom';
+
+import ProductHome from './home';
+import ProductAddUpdate from './add-update';
+import ProductDetail from './detail';
+
 
 export default class Product extends Component{
     render(){
         return(
-            <div>
-                Product
-            </div>
+            <Switch>
+                <Route path = '/product' component = {ProductHome} exact></Route>
+                <Route path = '/product/addupdate' component = {ProductAddUpdate}></Route>
+                <Route path = '/product/detail' component = {ProductDetail}></Route>
+                <Redirect to = '/product'/>
+            </Switch>
         )
     }
 }
